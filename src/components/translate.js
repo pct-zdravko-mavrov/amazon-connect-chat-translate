bf79981a8eac296914631795c4889b18a63107b7
pct-zdravko-mavrov <135987330+pct-zdravko-mavrov@users.noreply.github.com>
2024-07-04 14:41:44 +0300

    Update translate.js
    Adding formality setting to translation API

--- src/components/translate.js
+++ src/components/translate.js
@@ -1,19 +1,53 @@
-import  { Predictions} from '@aws-amplify/predictions';
+import { Predictions } from "@aws-amplify/predictions";
 
+async function ProcessChatText(
+  content,
+  sourceLang,
+  tagretLang,
+  formality = "FORMAL"
+) {
+  const formalitySupportedLanguages = [
+    "nl",
+    "fr",
+    "fr-CA",
+    "de",
+    "hi",
+    "it",
+    "ja",
+    "ko",
+    "pt-PT",
+    "es",
+    "es-MX",
+  ];
 
+  let translateOptions = {
+    source: {
+      text: content,
+      language: sourceLang,
+    },
+    targetLanguage: tagretLang,
+  };
 
-async function ProcessChatText(content, sourceLang, tagretLang) {
+  // Check if the target language supports formality customization
+  if (formalitySupportedLanguages.includes(targetLanguage)) {
+    translateOptions.formality = formality;
+  }
 
-    let transcriptMessage = await Predictions.convert({
-        translateText: {
-            source: {
-                text: content,
-                language: sourceLang, // defaults configured on aws-exports.js
-                // supported languages https://docs.aws.amazon.com/translate/latest/dg/how-it-works.html#how-it-works-language-codes
-            },
-            targetLanguage: tagretLang
-        }
-    });
-    return transcriptMessage.text
+  // let transcriptMessage = await Predictions.convert({
+  //     translateText: {
+  //         source: {
+  //             text: content,
+  //             language: sourceLang, // defaults configured on aws-exports.js
+  //             // supported languages https://docs.aws.amazon.com/translate/latest/dg/how-it-works.html#how-it-works-language-codes
+  //         },
+  //         targetLanguage: tagretLang
+  //     }
+  // });
+
+  let transcriptMessage = await Predictions.convert({
+    translateText: translateOptions,
+  });
+
+  return transcriptMessage.text;
 }
-export default ProcessChatText
+export default ProcessChatText;
